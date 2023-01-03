c272d3e5b4522f41652639c890a54e5fb44778ed
TerenceLGS <none@email>
2023-01-04 00:17:50 +1300

    Having issues with bindings

--- UiControls/MainSections/KeysControlViewModel.cs
+++ UiControls/MainSections/KeysControlViewModel.cs
@@ -5,10 +5,12 @@ namespace LaunchLightly.UiControls.MainSections;
 
 public class KeysControlViewModel : ViewModelBase {
 	
-	public string SdkKey { get; set; } = String.Empty;
+	public string SdkKey { get; set; } = "String.Empty";
 	public string ApiKey { get; set; } = String.Empty;
 	public string ProjectId { get; set; } = String.Empty;
 	public string EnvironmentKey { get; set; } = String.Empty;
 	public string FlagId { get; set; } = String.Empty;
-	public bool RevealKeys { get; set; } = true;
+	public bool? RevealKeys { get; set; } = true;
+
+	public string? PasswordChar => RevealKeys.GetValueOrDefault() ? null : "~";
 }
